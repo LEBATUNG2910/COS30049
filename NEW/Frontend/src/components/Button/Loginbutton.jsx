01d1e10227ae4a5edc4e48f3e6745f09bfec22a8
quynezz <laungocquyen098@gmail.com>
2024-03-06 16:15:36 +0700

    F

--- NEW/Frontend/src/components/Button/Loginbutton.jsx
+++ NEW/Frontend/src/components/Button/Loginbutton.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const LoginButton = () => {
   return (
     <div className="w-full h-40 flex items-center justify-center cursor-pointer">
-      <Link to="/Token">
+      <Link to="/Login">
         <div className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold shadow text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 dark:bg-gray-700 dark:text-white dark:hover:text-gray-200 dark:shadow-none group">
           <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-pink-500 group-hover:h-full"></span>
           <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
