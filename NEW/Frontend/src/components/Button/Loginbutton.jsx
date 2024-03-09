9c035fe09b2a3fe0fd3f1c6c86a6ce4326775a71
LEBATUNG2910 <bumleba2910@gmail.com>
2024-03-09 13:27:22 +0700

    Btung

--- NEW/Frontend/src/components/Button/Loginbutton.jsx
+++ NEW/Frontend/src/components/Button/Loginbutton.jsx
@@ -5,7 +5,7 @@ const LoginButton = () => {
   return (
     <div className="w-full h-40 flex items-center justify-center cursor-pointer">
       <Link to="/Login">
-        <button class="bg-blue-200 hover:bg-violet-400 text-black font-bold py-3 px-6 rounded-full shadow-lg shadow-neutral-950 hover:text-white transform transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110 hover:animate-pulse active:animate-bounce">
+        <button class="bg-white hover:bg-red-500 text-black font-bold py-3 px-6 rounded-full shadow-lg shadow-neutral-950 hover:text-white transform transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110  active:animate-bounce">
           Login
         </button>
       </Link>
